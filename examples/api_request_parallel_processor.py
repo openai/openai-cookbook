0e3ce04944d91f67cb0c9bfde7ebc926b2f5f54d
banda-larga <edoardofedericik@virgilio.it>
2023-03-14 15:18:54 +0100

    added chat format in num_token_consumed_from_request

--- examples/api_request_parallel_processor.py
+++ examples/api_request_parallel_processor.py
@@ -117,7 +117,9 @@ async def process_api_requests_from_file(
     """Processes API requests in parallel, throttling to stay under rate limits."""
     # constants
     seconds_to_pause_after_rate_limit_error = 15
-    seconds_to_sleep_each_loop = 0.001  # 1 ms limits max throughput to 1,000 requests per second
+    seconds_to_sleep_each_loop = (
+        0.001  # 1 ms limits max throughput to 1,000 requests per second
+    )
 
     # initialize logging
     logging.basicConfig(level=logging_level)
@@ -129,8 +131,12 @@ async def process_api_requests_from_file(
 
     # initialize trackers
     queue_of_requests_to_retry = asyncio.Queue()
-    task_id_generator = task_id_generator_function()  # generates integer IDs of 1, 2, 3, ...
-    status_tracker = StatusTracker()  # single instance to track a collection of variables
+    task_id_generator = (
+        task_id_generator_function()
+    )  # generates integer IDs of 1, 2, 3, ...
+    status_tracker = (
+        StatusTracker()
+    )  # single instance to track a collection of variables
     next_request = None  # variable to hold the next request to call
 
     # initialize available capacity counts
@@ -153,7 +159,9 @@ async def process_api_requests_from_file(
             if next_request is None:
                 if queue_of_requests_to_retry.empty() is False:
                     next_request = queue_of_requests_to_retry.get_nowait()
-                    logging.debug(f"Retrying request {next_request.task_id}: {next_request}")
+                    logging.debug(
+                        f"Retrying request {next_request.task_id}: {next_request}"
+                    )
                 elif file_not_finished:
                     try:
                         # get new request
@@ -161,12 +169,16 @@ async def process_api_requests_from_file(
                         next_request = APIRequest(
                             task_id=next(task_id_generator),
                             request_json=request_json,
-                            token_consumption=num_tokens_consumed_from_request(request_json, api_endpoint, token_encoding_name),
+                            token_consumption=num_tokens_consumed_from_request(
+                                request_json, api_endpoint, token_encoding_name
+                            ),
                             attempts_left=max_attempts,
                         )
                         status_tracker.num_tasks_started += 1
                         status_tracker.num_tasks_in_progress += 1
-                        logging.debug(f"Reading request {next_request.task_id}: {next_request}")
+                        logging.debug(
+                            f"Reading request {next_request.task_id}: {next_request}"
+                        )
                     except StopIteration:
                         # if file runs out, set flag to stop reading it
                         logging.debug("Read file exhausted")
@@ -176,11 +188,13 @@ async def process_api_requests_from_file(
             current_time = time.time()
             seconds_since_update = current_time - last_update_time
             available_request_capacity = min(
-                available_request_capacity + max_requests_per_minute * seconds_since_update / 60.0,
+                available_request_capacity
+                + max_requests_per_minute * seconds_since_update / 60.0,
                 max_requests_per_minute,
             )
             available_token_capacity = min(
-                available_token_capacity + max_tokens_per_minute * seconds_since_update / 60.0,
+                available_token_capacity
+                + max_tokens_per_minute * seconds_since_update / 60.0,
                 max_tokens_per_minute,
             )
             last_update_time = current_time
@@ -217,19 +231,32 @@ async def process_api_requests_from_file(
             await asyncio.sleep(seconds_to_sleep_each_loop)
 
             # if a rate limit error was hit recently, pause to cool down
-            seconds_since_rate_limit_error = (time.time() - status_tracker.time_of_last_rate_limit_error)
+            seconds_since_rate_limit_error = (
+                time.time() - status_tracker.time_of_last_rate_limit_error
+            )
             if seconds_since_rate_limit_error < seconds_to_pause_after_rate_limit_error:
-                remaining_seconds_to_pause = (seconds_to_pause_after_rate_limit_error - seconds_since_rate_limit_error)
+                remaining_seconds_to_pause = (
+                    seconds_to_pause_after_rate_limit_error
+                    - seconds_since_rate_limit_error
+                )
                 await asyncio.sleep(remaining_seconds_to_pause)
                 # ^e.g., if pause is 15 seconds and final limit was hit 5 seconds ago
-                logging.warn(f"Pausing to cool down until {time.ctime(status_tracker.time_of_last_rate_limit_error + seconds_to_pause_after_rate_limit_error)}")
+                logging.warn(
+                    f"Pausing to cool down until {time.ctime(status_tracker.time_of_last_rate_limit_error + seconds_to_pause_after_rate_limit_error)}"
+                )
 
         # after finishing, log final status
-        logging.info(f"""Parallel processing complete. Results saved to {save_filepath}""")
+        logging.info(
+            f"""Parallel processing complete. Results saved to {save_filepath}"""
+        )
         if status_tracker.num_tasks_failed > 0:
-            logging.warning(f"{status_tracker.num_tasks_failed} / {status_tracker.num_tasks_started} requests failed. Errors logged to {save_filepath}.")
+            logging.warning(
+                f"{status_tracker.num_tasks_failed} / {status_tracker.num_tasks_started} requests failed. Errors logged to {save_filepath}."
+            )
         if status_tracker.num_rate_limit_errors > 0:
-            logging.warning(f"{status_tracker.num_rate_limit_errors} rate limit errors received. Consider running at a lower rate.")
+            logging.warning(
+                f"{status_tracker.num_rate_limit_errors} rate limit errors received. Consider running at a lower rate."
+            )
 
 
 # dataclasses
@@ -285,7 +312,9 @@ class APIRequest:
                 if "Rate limit" in response["error"].get("message", ""):
                     status_tracker.time_of_last_rate_limit_error = time.time()
                     status_tracker.num_rate_limit_errors += 1
-                    status_tracker.num_api_errors -= 1  # rate limit errors are counted separately
+                    status_tracker.num_api_errors -= (
+                        1  # rate limit errors are counted separately
+                    )
 
         except Exception as e:  # catching naked exceptions is bad practice, but in this case we'll log & save them
             logging.warning(f"Request {self.task_id} failed with Exception {e}")
@@ -296,7 +325,9 @@ class APIRequest:
             if self.attempts_left:
                 retry_queue.put_nowait(self)
             else:
-                logging.error(f"Request {self.request_json} failed after all attempts. Saving errors: {self.result}")
+                logging.error(
+                    f"Request {self.request_json} failed after all attempts. Saving errors: {self.result}"
+                )
                 append_to_jsonl([self.request_json, self.result], save_filepath)
                 status_tracker.num_tasks_in_progress -= 1
                 status_tracker.num_tasks_failed += 1
@@ -331,20 +362,47 @@ def num_tokens_consumed_from_request(
     encoding = tiktoken.get_encoding(token_encoding_name)
     # if completions request, tokens = prompt + n * max_tokens
     if api_endpoint == "completions":
-        prompt = request_json["prompt"]
-        max_tokens = request_json.get("max_tokens", 15)
+
         n = request_json.get("n", 1)
-        completion_tokens = n * max_tokens
-        if isinstance(prompt, str):  # single prompt
-            prompt_tokens = len(encoding.encode(prompt))
-            num_tokens = prompt_tokens + completion_tokens
-            return num_tokens
-        elif isinstance(prompt, list):  # multiple prompts
-            prompt_tokens = sum([len(encoding.encode(p)) for p in prompt])
-            num_tokens = prompt_tokens + completion_tokens
-            return num_tokens
-        else:
-            raise TypeError('Expecting either string or list of strings for "prompt" field in completion request')
+        max_tokens = request_json.get("max_tokens", 15)
+
+        if request_json.get("prompt"):
+            prompt = request_json["prompt"]
+            completion_tokens = n * max_tokens
+            if isinstance(prompt, str):  # single prompt
+                prompt_tokens = len(encoding.encode(prompt))
+                num_tokens = prompt_tokens + completion_tokens
+                return num_tokens
+            elif isinstance(prompt, list):  # multiple prompts
+                prompt_tokens = sum([len(encoding.encode(p)) for p in prompt])
+                num_tokens = prompt_tokens + completion_tokens
+                return num_tokens
+            else:
+                raise TypeError(
+                    'Expecting either string or list of strings for "prompt" field in completion request'
+                )
+
+        elif request_json.get("messages"):
+            messages = request_json["messages"]
+            completion_tokens = n * max_tokens
+            if isinstance(messages, list):
+                messages_tokens = 0
+                for message in messages:
+                    messages_tokens += 4  # every message follows <im_start>{role/name}\n{content}<im_end>\n
+                    for key, value in message.items():
+                        messages_tokens += len(encoding.encode(value))
+                        if key == "name":  # if there's a name, the role is omitted
+                            messages_tokens += (
+                                -1
+                            )  # role is always required and always 1 token
+                messages_tokens += 2  # every reply is primed with <im_start>assistant
+                num_tokens = messages_tokens + completion_tokens
+                return num_tokens
+            else:
+                raise TypeError(
+                    'Expecting list of dicts for "messages" field in completion request'
+                )
+
     # if embeddings request, tokens = input tokens
     elif api_endpoint == "embeddings":
         input = request_json["input"]
@@ -355,10 +413,14 @@ def num_tokens_consumed_from_request(
             num_tokens = sum([len(encoding.encode(i)) for i in input])
             return num_tokens
         else:
-            raise TypeError('Expecting either string or list of strings for "inputs" field in embedding request')
+            raise TypeError(
+                'Expecting either string or list of strings for "inputs" field in embedding request'
+            )
     # more logic needed to support other API calls (e.g., edits, inserts, DALL-E)
     else:
-        raise NotImplementedError(f'API endpoint "{api_endpoint}" not implemented in this script')
+        raise NotImplementedError(
+            f'API endpoint "{api_endpoint}" not implemented in this script'
+        )
 
 
 def task_id_generator_function():

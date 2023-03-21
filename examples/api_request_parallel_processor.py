2502292abc454af683df2fd5d4da295ec0e96f39
Herobs <herobs@herobs.cn>
2023-03-21 08:02:58 +0800

    supplement comment

--- examples/api_request_parallel_processor.py
+++ examples/api_request_parallel_processor.py
@@ -341,9 +341,12 @@ def num_tokens_consumed_from_request(
         if api_endpoint.startswith("chat/"):
             num_tokens = 0
             for message in request_json["messages"]:
-                num_tokens += 4
+                num_tokens += 4  # every message follows <im_start>{role/name}\n{content}<im_end>\n
                 for key, value in message.items():
                     num_tokens += len(encoding.encode(value))
+                    if key == "name":  # if there's a name, the role is omitted
+                        num_tokens -= 1  # role is always required and always 1 token
+            num_tokens += 2  # every reply is primed with <im_start>assistant
             return num_tokens + completion_tokens
         # normal completions
         else:

f1ae3070d6628fd445ad3537586fd2851834d288
VyNAdastra <vy.nguyen@adastragrp.com>
2023-05-11 01:08:16 -0400

    reformat code

--- get_functions.py
+++ get_functions.py
@@ -5,19 +5,17 @@ from typing import List, Tuple, Any
 import json
 import ast
 import csv
+
+
 def recursive_items(dictionary):
     for key, value in dictionary.items():
         if type(value) is dict:
             if value:
-                return key+',' + ''.join(recursive_items(value))
+                return key + "," + "".join(recursive_items(value))
             else:
                 return (key, value)
         else:
             return (key, value)
-
-
-
-
 
 
 def getmembers(item: Any, predicate=None) -> List[Tuple[str, Any]]:
@@ -33,7 +31,7 @@ def getmembers(item: Any, predicate=None) -> List[Tuple[str, Any]]:
     return inspect.getmembers(item, predicate)
 
 
-def import_all_modules(directory: str, object_dict,directory_dict) -> None:
+def import_all_modules(directory: str, object_dict, directory_dict) -> None:
     """
     Import all modules from a directory.
 
@@ -42,17 +40,15 @@ def import_all_modules(directory: str, object_dict,directory_dict) -> None:
     """
 
     for current_file in os.listdir(directory):
-
         filepath = os.path.join(directory, current_file)
         if os.path.isdir(filepath):
             # Recursively call the function for subdirectories
-            import_all_modules(filepath, object_dict,directory_dict)
-        elif current_file.endswith('.json'):
-           
-            with open(filepath, 'r') as f:
+            import_all_modules(filepath, object_dict, directory_dict)
+        elif current_file.endswith(".json"):
+            with open(filepath, "r") as f:
                 data = json.load(f)
 
-    # Get the first and second level keys
+            # Get the first and second level keys
             keys = set()
             for key1 in data:
                 keys.add(key1)
@@ -62,67 +58,64 @@ def import_all_modules(directory: str, object_dict,directory_dict) -> None:
 
             # Convert the set of keys to a string
             key_string = "\n".join(keys)
-            directory_dict[filepath+'key_string']=key_string
-        elif current_file.endswith('.csv'):
-            
-
-# Open the CSV file for reading
-            with open(filepath, newline='') as f:
+            directory_dict[filepath + "key_string"] = key_string
+        elif current_file.endswith(".csv"):
+            # Open the CSV file for reading
+            with open(filepath, newline="") as f:
                 reader = csv.reader(f)
 
                 # Read the first row of the CSV file, which contains the column names
                 column_names = next(reader)
 
             # Print the column names
-            directory_dict[filepath+'column_names']=column_names
-        elif current_file.endswith(".py") and current_file != '__init__.py':
-            with open(filepath, 'r') as f:
+            directory_dict[filepath + "column_names"] = column_names
+        elif current_file.endswith(".py") and current_file != "__init__.py":
+            with open(filepath, "r") as f:
                 code = f.read()
             object_dict[filepath] = {}
             directory_dict[filepath] = {}
-            object_dict[filepath]['objects'] = {}
-            directory_dict[filepath]['objects'] = {}
+            object_dict[filepath]["objects"] = {}
+            directory_dict[filepath]["objects"] = {}
             tree = ast.parse(code)
-            import_str = ''
+            import_str = ""
             for node in tree.body:
                 if isinstance(node, ast.Import):
                     import_str += f"import {', '.join([alias.name for alias in node.names])}"
                 elif isinstance(node, ast.ImportFrom):
                     import_str += f"from {node.module} import {', '.join([alias.name for alias in node.names])}"
             module_name = current_file[:-3]
-            spec = importlib.util.spec_from_file_location(
-                module_name, os.path.join(directory, current_file))
+            spec = importlib.util.spec_from_file_location(module_name, os.path.join(directory, current_file))
             module = importlib.util.module_from_spec(spec)
             spec.loader.exec_module(module)
             members = getmembers(module, inspect.isfunction)
             for member in members:
-                if not 'import_str' in object_dict[filepath]:
-                    object_dict[filepath]['import_str'] = import_str
-                    directory_dict[filepath]['functions']=[]
+                if not "import_str" in object_dict[filepath]:
+                    object_dict[filepath]["import_str"] = import_str
+                    directory_dict[filepath]["functions"] = []
                 if member[1].__module__ == module.__name__:
-                    object_dict[filepath]['objects'][member[0]] = member[1]
-                    directory_dict[filepath]['functions'].append(member[0])
+                    object_dict[filepath]["objects"][member[0]] = member[1]
+                    directory_dict[filepath]["functions"].append(member[0])
             classes = [m[1] for m in getmembers(module, inspect.isclass)]
 
             for class_ in classes:
-                object_dict[filepath]['objects'][class_] = {}
-                directory_dict[filepath]['objects'][class_] = []
+                object_dict[filepath]["objects"][class_] = {}
+                directory_dict[filepath]["objects"][class_] = []
                 members = getmembers(class_, inspect.isfunction)
                 for member in members:
                     if member[1].__module__ == module.__name__:
                         if object_dict[filepath]:
-                            if not 'import_str' in object_dict[filepath]:
-                                object_dict[filepath]['import_str'] = import_str
-                            directory_dict[filepath]['objects'][class_].append(member[0])
-                            object_dict[filepath]['objects'][class_][member[0]] = member[1]
+                            if not "import_str" in object_dict[filepath]:
+                                object_dict[filepath]["import_str"] = import_str
+                            directory_dict[filepath]["objects"][class_].append(member[0])
+                            object_dict[filepath]["objects"][class_][member[0]] = member[1]
 
                     # else:
-                        # print(f"Skipping imported method: {member[0]}")
-    return object_dict,directory_dict
+                    # print(f"Skipping imported method: {member[0]}")
+    return object_dict, directory_dict
 
 
 if __name__ == "__main__":
     output_dict = {}
     directory_dict = {}
-    output_dict,directory_dict = import_all_modules("test_code", output_dict,directory_dict)
+    output_dict, directory_dict = import_all_modules("test_code", output_dict, directory_dict)
     print(directory_dict)
--- main.py
+++ main.py
@@ -6,6 +6,7 @@ from get_functions import import_all_modules
 import re
 import subprocess
 import inspect
+
 # example of a function that uses a multi-step prompt to write unit tests
 
 
@@ -19,9 +20,9 @@ def unit_test_from_function(
     reruns_if_fail: int = 0,
 ) -> str:
     """Outputs a unit test for a given Python function, using a 3-step GPT-3 prompt."""
-    
-# the init function for this classs is\
-# {init_str}\
+
+    # the init function for this classs is\
+    # {init_str}\
 
     plan_response = openai.ChatCompletion.create(
         engine=engine,
@@ -32,7 +33,7 @@ def unit_test_from_function(
         stream=False,
     )
     unit_test_completion = plan_response.choices[0].message.content
-    input_messages.append( {"role": "assistant", "content":plan_response.choices[0].message.content })
+    input_messages.append({"role": "assistant", "content": plan_response.choices[0].message.content})
     # check the output for errors
     code_output = extract_all_python_code(unit_test_completion)
     try:
@@ -49,17 +50,17 @@ def unit_test_from_function(
                 temperature=temperature,
                 reruns_if_fail=reruns_if_fail - 1,  # decrement rerun counter when calling again
             )
-    return code_output,input_messages
+    return code_output, input_messages
 
 
 def extract_all_python_code(string):
-    start_marker = '```python'
-    end_marker = '```'
-    pattern = re.compile(f'{start_marker}(.*?)({end_marker})', re.DOTALL)
+    start_marker = "```python"
+    end_marker = "```"
+    pattern = re.compile(f"{start_marker}(.*?)({end_marker})", re.DOTALL)
     matches = pattern.findall(string)
     if not matches:
         return None
-    return ''.join([match[0].strip() for match in matches])
+    return "".join([match[0].strip() for match in matches])
 
 
 if __name__ == "__main__":
@@ -69,6 +70,7 @@ if __name__ == "__main__":
     load_dotenv()
     import os
     import openai
+
     openai.api_type = "azure"
     openai.api_base = "https://aoaihackathon.openai.azure.com/"
     openai.api_version = "2023-03-15-preview"
@@ -77,10 +79,12 @@ if __name__ == "__main__":
     model = "gpt-4"
     object_dict = {}
     directory_dict = {}
-    object_dict,directory_dict = import_all_modules("test_code", object_dict=object_dict,directory_dict=directory_dict)
+    object_dict, directory_dict = import_all_modules(
+        "test_code", object_dict=object_dict, directory_dict=directory_dict
+    )
     i = 0
     messages = []
-    unit_test_package = 'pytest'
+    unit_test_package = "pytest"
     platform = "python 3.9"
     user_input = "This repo uses the dq_utility to check the data quality based on different sources given in\
         csv files like az_ca_pcoe_dq_rules_innomar, it will generate a csv output of the lines with errors in a csv file, to use the repo u can:\
@@ -96,35 +100,34 @@ if __name__ == "__main__":
     repo_explanation = f"you are providing unit test using {unit_test_package}` and Python 3.9\
         {user_input}. to give details about the structure of the repo look at the dictionary below, it includes all files\
         and if python, all function and calsses, if json first and second level keys and if csv, the column names :{directory_dict}"
-    messages.append({"role": "system",
-            "content": repo_explanation})
+    messages.append({"role": "system", "content": repo_explanation})
     for file_path, objects in object_dict.items():
-        if 'objects' in objects:
-            for obj_key, obj_value in objects['objects'].items():
+        if "objects" in objects:
+            for obj_key, obj_value in objects["objects"].items():
                 if obj_value:
-                    if type(obj_value)==dict:
-                        for class_method_name,class_method in obj_value.items():
+                    if type(obj_value) == dict:
+                        for class_method_name, class_method in obj_value.items():
                             # if '__init__' in str(class_method).split(' at ')[0].replace('<','').replace('.','_').replace(' ','_'):
                             #     init_str = inspect.getsource(class_method)
                             #     continue
                             # if __init__function:
                             #     init_str=f'the __init__ function for this class is: {__init__function}'\
-                            class_name = str(class_method).split('.')[0].split(' ')[1]
-                            function_name = str(class_method).split('.')[1].split(' ')[0]
-                            function_to_test= inspect.getsource(class_method)
+                            class_name = str(class_method).split(".")[0].split(" ")[1]
+                            function_name = str(class_method).split(".")[1].split(" ")[0]
+                            function_to_test = inspect.getsource(class_method)
                             prompt_to_explain_the_function = f" provide unit test for the function `{function_name} in {class_name}`.\
                                 ```python\
                                 {function_to_test} \
                                 ```\
                                 and the path of the function is {file_path}\
                                 "
-                            messages.append({"role": "user","content":prompt_to_explain_the_function})
-                            unit_test_code,messages = unit_test_from_function(
-                            input_messages=messages,
-                            engine=engine,
-                            model=model,
-                        )
-                            current_test_path = f'test_code/unit_test/test_{class_method_name}.py'
+                            messages.append({"role": "user", "content": prompt_to_explain_the_function})
+                            unit_test_code, messages = unit_test_from_function(
+                                input_messages=messages,
+                                engine=engine,
+                                model=model,
+                            )
+                            current_test_path = f"test_code/unit_test/test_{class_method_name}.py"
                             with open(current_test_path, "w") as test_f:
                                 test_f.write(unit_test_code)
                     else:
@@ -134,7 +137,9 @@ if __name__ == "__main__":
                             engine=engine,
                             model=model,
                         )
-                        current_test_path = f"test_code/unit_test/test_{str(obj_value).split(' at ')[0].replace('<','')}.py"
+                        current_test_path = (
+                            f"test_code/unit_test/test_{str(obj_value).split(' at ')[0].replace('<','')}.py"
+                        )
                         with open(current_test_path, "w") as test_f:
                             test_f.write(unit_test_code)
                     MAX_RETRIES = 3
@@ -143,5 +148,5 @@ if __name__ == "__main__":
                     i += 1
                     if i > 3:
                         break
-            
+
         # print('done')

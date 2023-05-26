be3759ed0f35e72d96b1f07e08958ec51f431341
1368170336@qq.com <1368170336@qq.com>
2023-05-26 16:41:41 +0800

    replace search with embedding

--- examples/fine-tuned_qa/answers_with_ft.py
+++ examples/fine-tuned_qa/answers_with_ft.py
@@ -1,49 +1,73 @@
-"""
-Note: To answer questions based on text documents, we recommend the procedure in 
-[Question Answering using Embeddings](https://github.com/openai/openai-cookbook/blob/main/examples/Question_answering_using_embeddings.ipynb).
-Some of the code below may rely on [deprecated API endpoints](https://github.com/openai/openai-cookbook/tree/main/transition_guides_for_deprecated_API_endpoints).
-"""
-
-import argparse
-
+import ast
 import openai
+import argparse
+import tiktoken
+import pandas as pd
+from scipy import spatial
+
+
+def num_tokens(text: str, model: str) -> int:
+    """Return the number of tokens in a string."""
+    encoding = tiktoken.encoding_for_model(model)
+    return len(encoding.encode(text))
+
+
+def strings_ranked_by_relatedness(
+    query: str,
+    df: pd.DataFrame,
+    model: str,
+    relatedness_fn=lambda x, y: 1 - spatial.distance.cosine(x, y),
+    top_n: int = 100
+) -> tuple[list[str], list[float]]:
+    """Returns a list of strings and relatednesses, sorted from most related to least."""
+    query_embedding_response = openai.Embedding.create(
+        model=model,
+        input=query,
+    )
+    query_embedding = query_embedding_response["data"][0]["embedding"]
+    strings_and_relatednesses = [
+        (row["text"], relatedness_fn(query_embedding, row["embedding"]))
+        for i, row in df.iterrows()
+    ]
+    strings_and_relatednesses.sort(key=lambda x: x[1], reverse=True)
+    strings, relatednesses = zip(*strings_and_relatednesses)
+    return strings[:top_n], relatednesses[:top_n]
 
 
 def create_context(
-    question, search_file_id, max_len=1800, search_model="ada", max_rerank=10
+    question, df, max_len=1800, embed_model="text-embedding-ada-002",
+    gpt_model="gpt-3.5-turbo", max_rerank=10
 ):
     """
     Create a context for a question by finding the most similar context from the search file.
     :param question: The question
-    :param search_file_id: The file id of the search file
+    :param df: The dataframe of relevant texts and embeddings
     :param max_len: The maximum length of the returned context (in tokens)
-    :param search_model: The search model to use
+    :param embed_model: The embed model to use
+    :param gpt_model: The gpt model to use
     :param max_rerank: The maximum number of reranking
     :return: The context
     """
-    results = openai.Engine(search_model).search(
-        search_model=search_model,
-        query=question,
-        max_rerank=max_rerank,
-        file=search_file_id,
-        return_metadata=True,
-    )
-    returns = []
-    cur_len = 0
-    for result in results["data"]:
-        cur_len += int(result["metadata"]) + 4
-        if cur_len > max_len:
+    strings, _ = strings_ranked_by_relatedness(question, df, model=embed_model, top_n=max_rerank)
+    message = ""
+    for string in strings:
+        next_article = f'{string}\n\n###\n\n'
+        if (
+            num_tokens(message + next_article, model=gpt_model) > max_len
+        ):
             break
-        returns.append(result["text"])
-    return "\n\n###\n\n".join(returns)
+        else:
+            message += next_article
+    return message
 
 
 def answer_question(
-    search_file_id="<SEARCH_FILE_ID>",
+    df="<DATAFRAME_OF_TEXTS_AND_EMBEDDINGS>",
     fine_tuned_qa_model="<FT_QA_MODEL_ID>",
     question="Which country won the European Football championship in 2021?",
     max_len=1800,
-    search_model="ada",
+    embed_model="text-embedding-ada-002",
+    gpt_model="gpt-3.5-turbo",
     max_rerank=10,
     debug=False,
     stop_sequence=["\n", "."],
@@ -53,9 +77,10 @@ def answer_question(
     Answer a question based on the most similar context from the search file, using your fine-tuned model.
     :param question: The question
     :param fine_tuned_qa_model: The fine tuned QA model
-    :param search_file_id: The file id of the search file
+    :param df: The dataframe of texts and embeddings to search
     :param max_len: The maximum length of the returned context (in tokens)
-    :param search_model: The search model to use
+    :param embed_model: The embed model to use
+    :param gpt_model: The gpt model to use
     :param max_rerank: The maximum number of reranking
     :param debug: Whether to output debug information
     :param stop_sequence: The stop sequence for Q&A model
@@ -64,9 +89,10 @@ def answer_question(
     """
     context = create_context(
         question,
-        search_file_id,
+        df,
         max_len=max_len,
-        search_model=search_model,
+        embed_model=embed_model,
+        gpt_model=gpt_model,
         max_rerank=max_rerank,
     )
     if debug:
@@ -102,7 +128,10 @@ if __name__ == "__main__":
         formatter_class=argparse.ArgumentDefaultsHelpFormatter,
     )
     parser.add_argument(
-        "--search_file_id", help="Search file id", required=True, type=str
+        "--embeddings_path",
+        help="Embeddings CSV file path",
+        default="https://cdn.openai.com/API/examples/data/winter_olympics_2022.csv",
+        type=str
     )
     parser.add_argument(
         "--fine_tuned_qa_model", help="Fine-tuned QA model id", required=True, type=str
@@ -117,7 +146,10 @@ if __name__ == "__main__":
         type=int,
     )
     parser.add_argument(
-        "--search_model", help="Search model to use", default="ada", type=str
+        "--embed_model", help="Embed model to use", default="text-embedding-ada-002", type=str
+    )
+    parser.add_argument(
+        "--gpt_model", help="GPT model to use", default="gpt-3.5-turbo", type=str
     )
     parser.add_argument(
         "--max_rerank",
@@ -142,12 +174,15 @@ if __name__ == "__main__":
         type=int,
     )
     args = parser.parse_args()
+    df = pd.read_csv(args.embeddings_path)
+    df['embedding'] = df['embedding'].apply(ast.literal_eval)
     response = answer_question(
-        search_file_id=args.search_file_id,
+        df=df,
         fine_tuned_qa_model=args.fine_tuned_qa_model,
         question=args.question,
         max_len=args.max_len,
-        search_model=args.search_model,
+        embed_model=args.embed_model,
+        gpt_model=args.gpt_model,
         max_rerank=args.max_rerank,
         debug=args.debug,
         stop_sequence=args.stop_sequence,

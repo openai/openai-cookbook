76454f2456b36a65e1b4e9cde4bfebab61650076
ayush rajgor <ayushrajgorar@gmail.com>
2024-01-05 14:49:59 +0530

    revert to main

--- examples/vector_databases/kusto/Getting_started_with_kusto_and_openai_embeddings_cell24.py
+++
@@ -1,8 +0,0 @@
-def embed(query):
-    # Creates embedding vector from user query
-    embedded_query = openai.embeddings.create(
-            input=query,
-            deployment_id="embed", #replace with your deployment id
-            chunk_size=1
-    )["data"][0]["embedding"]
-    return embedded_query

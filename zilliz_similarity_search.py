4bed16b3b0ee5a4e4349fa427bb62bc59b7954d6
Rasmus Storjohann <rasmus@peacegeeks.org>
2024-04-01 14:20:00 -0700

    CleanUp

--- zilliz_similarity_search.py
+++ zilliz_similarity_search.py
@@ -5,34 +5,32 @@ import sys
 import os
 from dotenv import load_dotenv
 
-EMBED_MODEL = "all-MiniLM-L6-v2"
-embedding_func = embedding_functions.SentenceTransformerEmbeddingFunction(model_name=EMBED_MODEL)
+load_dotenv()
+ZILLIZ_ENDPOINT = os.getenv('ZILLIZ_ENDPOINT')
+ZILLIZ_API_KEY = os.getenv('ZILLIZ_API_KEY')
+COLLECTION_NAME = 'noc_data_cosine_all_MiniLM_L6_v2_with_384_dimensions'
 
 def read_job_description(job):
     with open("./data/jobdescriptions/" + job + ".txt") as file:
         return file.read()
 
+EMBED_MODEL = "all-MiniLM-L6-v2"
+embedding_func = embedding_functions.SentenceTransformerEmbeddingFunction(model_name=EMBED_MODEL)
+
 job_description = read_job_description(sys.argv[1])
 
-vector = embedding_func(input=[job_description])[0]
-
-load_dotenv()
-ZILLIZ_ENDPOINT = os.getenv('ZILLIZ_ENDPOINT')
-ZILLIZ_USER = os.getenv('ZILLIZ_USER')
-ZILLIZ_PASSWORD = os.getenv('ZILLIZ_PASSWORD')
-ZILLIZ_BEARER = os.getenv('ZILLIZ_BEARER')
-COLLECTION_NAME = 'noc_data_cosine_all_MiniLM_L6_v2_with_384_dimensions'
+job_description_vector = embedding_func(input=[job_description])[0]
 
 url = ZILLIZ_ENDPOINT + '/v1/vector/search'
 
 data = json.dumps({
     'collectionName': COLLECTION_NAME,
-    'vector': vector,
+    'vector': job_description_vector,
     'outputFields': ['noc_code', 'title', 'definition']
 })
 
 headers = {
-    "Authorization": "Bearer " + ZILLIZ_BEARER,
+    "Authorization": "Bearer " + ZILLIZ_API_KEY,
     "Accept": "application/json",
     "Content-Type": "application/json"
 }

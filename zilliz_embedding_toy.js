183b24e82a8077ad354f11d87ca7ff077a425881
Rasmus Storjohann <rasmus@peacegeeks.org>
2024-04-02 13:40:45 -0700

    step

--- zilliz_embedding_toy.js
+++ zilliz_embedding_toy.js
@@ -1,11 +1,20 @@
 const { DefaultEmbeddingFunction } = require('chromadb');
 const json = require('json');
+const fs = require('fs');
+
+const JOB_DESCRIPTION_FOLDER = './data/jobdescriptions/';
+
+function readJobDescription(job) {
+    return fs.readFileSync(JOB_DESCRIPTION_FOLDER + job + '.txt', 'utf8');
+}
+
+const job_description = readJobDescription(process.argv[2]);
 
 async function doIt() {
     console.log('hello0');
     const embedder = new DefaultEmbeddingFunction('all-MiniLM-L6-v2');
     console.log('hello1');
-    return embedder.generate(['document1', 'document2']).then((result) => {
+    return embedder.generate([job_description]).then((result) => {
         console.log('hello2');
         console.log(result);
         console.log('hello3');

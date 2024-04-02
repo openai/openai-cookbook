40369ddd241f01ac8464ddf2db1360ae7005f43c
Rasmus Storjohann <rasmus@peacegeeks.org>
2024-04-02 13:37:54 -0700

    step

--- zilliz_embedding_toy.js
+++ zilliz_embedding_toy.js
@@ -1,28 +1,5 @@
 const { DefaultEmbeddingFunction } = require('chromadb');
-
-/*
-function helloWorld() {
-  return new Promise(resolve => {
-    setTimeout(() => {
-      resolve('Hello World!');
-    }, 2000);
-  });
-}
-
-const msg = async function() { //Async Function Expression
-  const msg = await helloWorld();
-  console.log('Message:', msg);
-}
-
-const msg1 = async () => { //Async Arrow Function
-  const msg = await helloWorld();
-  console.log('Message:', msg);
-}
-
-msg(); // Message: Hello World! <-- after 2 seconds
-msg1(); // Message: Hello World
-
-*/
+const json = require('json');
 
 async function doIt() {
     console.log('hello0');

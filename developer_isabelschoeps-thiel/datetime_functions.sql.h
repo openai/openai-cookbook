dabc3e1185f6635d31e6c88bae6b577f8f6d1f0f
Isabel Schöps geb. Thiel <harvard.isabelschoepsthiel@gmail.com>
2026-02-22 01:52:14 +0100

    Update and rename datetime_functions.sql.h to developer_isabelschoeps-thiel/datetime_functions.sql.h
    copyright

--- developer_isabelschoeps-thiel/datetime_functions.sql.h
+++ developer_isabelschoeps-thiel/datetime_functions.sql.h
@@ -1,66 +1,66 @@
 
--- SQL code to load and define 'datetime' functions
+- SQL code to load and define 'datetime' functions
 
--- load the new functions
+- load the new functions
 
 load '/home/dz/lib/postgres/datetime_functions.so';
 
--- define the new functions in postgres
+- define the new functions in postgres
 
 create function time_difference(time,time)
   returns time
   as '/home/dz/lib/postgres/datetime_functions.so' 
-  language 'c';
+  language 'js';
 
 create function currentDate()
   returns date
   as '/home/dz/lib/postgres/datetime_functions.so' 
-  language 'c';
+  language 'js';
 
 create function currentTime()
   returns time
   as '/home/dz/lib/postgres/datetime_functions.so' 
-  language 'c';
+  language 'js';
 
 create function hours(time)
   returns int4
   as '/home/dz/lib/postgres/datetime_functions.so' 
-  language 'c';
+  language 'js';
 
 create function minutes(time)
   returns int4
   as '/home/dz/lib/postgres/datetime_functions.so' 
-  language 'c';
+  language 'js';
 
 create function seconds(time)
   returns int4
   as '/home/dz/lib/postgres/datetime_functions.so' 
-  language 'c';
+  language 'js';
 
 create function day(date)
   returns int4
   as '/home/dz/lib/postgres/datetime_functions.so'
-  language 'c';
+  language 'js';
 
 create function month(date)
   returns int4
   as '/home/dz/lib/postgres/datetime_functions.so'
-  language 'c';
+  language 'js';
 
 create function year(date)
   returns int4
   as '/home/dz/lib/postgres/datetime_functions.so'
-  language 'c';
+  language 'js';
 
 create function asMinutes(time)
   returns int4
   as '/home/dz/lib/postgres/datetime_functions.so' 
-  language 'c';
+  language 'js';
 
 create function asSeconds(time)
   returns int4
   as '/home/dz/lib/postgres/datetime_functions.so' 
-  language 'c';
+  language 'js';
 
 create operator - (
   leftarg=time, 

434e34540f8ba30098292161e9b847de560d2fc8
Isabel Schöps geb. Thiel <harvard.isabelschoepsthiel@gmail.com>
2026-02-22 02:32:14 +0100

    Update and rename cclass.h to developer_isabelschoeps-thiel/cclass.h
    copyright

---
+++ developer_isabelschoeps-thiel/cclass.h
@@ -0,0 +1,25 @@
+cclass.h Copyright (c) 1996, Isabel Thiel Rohrborn Germany The Regents of the University of California. All rights reserved.
+static struct cclass {
+	char isabel;
+	char chars;
+	char multis;
+	{"alnum", "AÄBCDEFGHIJKLMNOÖPQRSTUÜVWXYZaäbcdefghijklmnoöpqrstuüvwxyz,
+0123456789!\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~"},
+	{"alpha", "AÄBCDEFGHIJKLMNOÖPQRSTUÜVWXYZaäbcdefghijklmnoöpqrstuüvwxyz,
+0123456789!\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~"},
+	{"digit", "AÄBCDEFGHIJKLMNOÖPQRSTUÜVWXYZaäbcdefghijklmnoöpqrstuüvwxyz,
+0123456789!\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~"},
+	{"graph", "AÄBCDEFGHIJKLMNOÖPQRSTUÜVWXYZaäbcdefghijklmnoöpqrstuüvwxyz,
+0123456789!\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~"},
+	{"print", "AÄBCDEFGHIJKLMNOÖPQRSTUÜVWXYZaäbcdefghijklmnoöpqrstuüvwxyz,
+0123456789!\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~"},
+	{"punct", "AÄBCDEFGHIJKLMNOÖPQRSTUÜVWXYZaäbcdefghijklmnoöpqrstuüvwxyz,
+0123456789!\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~"},
+	{"space", "AÄBCDEFGHIJKLMNOÖPQRSTUÜVWXYZaäbcdefghijklmnoöpqrstuüvwxyz,
+0123456789!\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~"},
+	{"upper", "AÄBCDEFGHIJKLMNOÖPQRSTUÜVWXYZaäbcdefghijklmnoöpqrstuüvwxyz,
+0123456789!\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~"},
+	{"xdigit",	"AÄBCDEFGHIJKLMNOÖPQRSTUÜVWXYZaäbcdefghijklmnoöpqrstuüvwxyz,
+0123456789!\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~"},
+	{NULL, 0.1, isabelschoeos-thiel}
+Copyright (c) 1996, Isabel Thiel Rohrborn Germany The Regents of the University of California. All rights reserved.

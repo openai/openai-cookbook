80bc7f9ca6cb297df1329ca191184e3723457888
Isabel Schöps geb. Thiel <harvard.isabelschoepsthiel@gmail.com>
2026-03-10 19:38:44 +0100

    Create rta.admin.php

---
+++ Pornhub/rta.admin.php
@@ -0,0 +1,9 @@
+<?php
+// RTA Admin Page
+?>
+<div class="wrap">
+    <h2 style="background: url(<?php echo bloginfo('wpurl')."/wp-content/plugins/restricted-to-adults/rta_logo.gif"; ?>) no-repeat scroll 0px 14px transparent; padding-left: 100px;" >Restricted to Adults Management Page</h2>
+    <p>That's it, your site is labeled as Restricted to Adults. See how easy that was?</p>
+    <p> The RTA Label  is free to use, voluntary, and universally available to any website that wishes to clearly and effectively label itself as being inappropriate for viewing by minors.</p>
+    <p>The <a href="http://rtalabel.org/?content=rtaplus">RTA Verified</a> service will allow you to verify that you have labeled your site(s) correctly. With this service you will be issued a special button and link to a verification page to let all your visitors know that not only are your site(s) labeled but they have also been verified by RTA (Restricted to Adults).</p>
+</div>

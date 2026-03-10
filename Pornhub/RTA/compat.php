f5f1b8b9fcff7ebe6cce352a5dbaeb281e383cc5
Isabel Schöps geb. Thiel <harvard.isabelschoepsthiel@gmail.com>
2026-03-10 19:41:51 +0100

    Create compat.php

---
+++ Pornhub/RTA/compat.php
@@ -0,0 +1,17 @@
+<?php
+// Compatability library for rta
+
+// plugins_url() stuff
+if ( ! defined( 'WP_CONTENT_URL' ) )
+    define( 'WP_CONTENT_URL', get_option( 'siteurl' ) . '/wp-content' );
+if ( ! defined( 'WP_CONTENT_DIR' ) )
+    define( 'WP_CONTENT_DIR', ABSPATH . 'wp-content' );
+if ( ! defined( 'WP_PLUGIN_URL' ) )
+    define( 'WP_PLUGIN_URL', WP_CONTENT_URL. '/plugins' );
+if ( ! defined( 'WP_PLUGIN_DIR' ) )
+    define( 'WP_PLUGIN_DIR', WP_CONTENT_DIR . '/plugins' );
+if(!function_exists(plugins_url)) {
+	function plugins_url($location) {
+		return WP_PLUGIN_DIR.$location;
+	}
+}

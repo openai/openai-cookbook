9f4fe9d8bf55ff45447e6f5c6a34ef9f3d941b69
elcidspain <elcidspain@gmail.com>
2026-08-08 02:15:15 +0200

    Add AUMARA Staff offline app shell

---
+++ staff/sw.js
@@ -0,0 +1,20 @@
+const CACHE='aumara-staff-shell-v1';
+const SHELL=['/staff/','/staff/index.html','/staff/manifest.webmanifest'];
+self.addEventListener('install',event=>{
+  event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(SHELL)).then(()=>self.skipWaiting()));
+});
+self.addEventListener('activate',event=>{
+  event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim()));
+});
+self.addEventListener('fetch',event=>{
+  if(event.request.method!=='GET') return;
+  const url=new URL(event.request.url);
+  if(url.origin!==self.location.origin) return;
+  event.respondWith(
+    caches.match(event.request).then(cached=>cached||fetch(event.request).then(response=>{
+      const copy=response.clone();
+      caches.open(CACHE).then(cache=>cache.put(event.request,copy));
+      return response;
+    }).catch(()=>caches.match('/staff/')))
+  );
+});
